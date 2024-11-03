c79f846a03ab6c50b09338d22f56391c009344bc
Cape23180909 <samilgamer@outlook.com>
2024-11-03 12:24:56 -0400

    Add project files.

---
+++ Tecnicos.Domain/DTO/ClientesDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Domain.DTO;
+public class ClientesDTO
+{
+    public int ClienteId { get; set; }
+    public string? Nombres { get; set; }
+    public string? WhatsApp { get; set; }
+}
